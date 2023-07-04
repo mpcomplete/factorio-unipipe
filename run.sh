9b6991febf391c6ab2960c44721855221b453e1e
Matt Perry <mpcomplete@gmail.com>
2023-07-03 21:42:35 -0400

    Bump v1.0.4

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-name=Unipipe_1.0.3
+name=Unipipe_1.0.4
 ./zip.sh
 mv ${name}.zip 'C:\Users\mpcom\AppData\Roaming\Factorio\mods'
 echo cmd /c '"C:\Program Files\Factorio\bin\x64\factorio.exe"'
